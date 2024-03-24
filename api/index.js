7998a36a519234c3a37a947783f754d78796c180
samyakkhatua <samyakkhatua@duck.com>
2024-03-24 13:07:48 +0530

    200

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send(`
+    res.status(200).send(`
       <html>
         <head>
           <title>EVENT BURST</title>
