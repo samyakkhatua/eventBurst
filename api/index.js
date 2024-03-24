9e3f3a060a55ae0c46692f36a00a135e5ad96dc6
samyakkhatua <samyakkhatua@duck.com>
2024-03-24 12:56:46 +0530

    fevicon

--- api/index.js
+++ api/index.js
@@ -14,6 +14,10 @@ app.get('/', (req, res) => {
       <html>
         <head>
           <title>EVENT BURST</title>
+          <link rel="apple-touch-icon" sizes="180x180" href="fevicon/apple-touch-icon.png">
+          <link rel="icon" type="image/png" sizes="32x32" href="fevicon/favicon-32x32.png">
+          <link rel="icon" type="image/png" sizes="16x16" href="fevicon/favicon-16x16.png">
+          <link rel="manifest" href="fevicon/site.webmanifest">
         </head>
         <body>
           <h1>Welcome to the Event Ticket Generator!</h1>
